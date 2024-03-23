779d7d78f048aea4cbbc1fef5cd320688727ec3a
Jeff Evans <jeffrey.wayne.evans@gmail.com>
2024-03-23 12:55:40 -0500

    Switch from build-clj library to using tools.build api instead
    Fix some deps issues in the various submodules
    
    Add an implementation of run-tests that simply forks clj to do it
    
    Update build-all.sh script to do the new stuff

--- type-handlers/avro/build.clj
+++ type-handlers/avro/build.clj
@@ -1,28 +1,42 @@
 (ns build
-  (:refer-clojure :exclude [test])
-  (:require [org.corfield.build :as bb]))
+  (:require [clojure.tools.build.api :as b]))
 
 (def lib 'net.clojars.jeff_evans/kc-repl-type-handler-avro)
-(def version "1.1.0")
+(def version (format "1.1.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def src-jar-file (format "target/%s-%s-avro-handler-src.jar" (name lib) version))
+(def uberjar-file (format "target/%s-%s-avro-handler.jar" (name lib) version))
+(def basis (delay (b/create-basis {:project "deps.edn"})))
 
-(defn test "Run the tests." [opts]
-  (bb/run-tests opts))
+(defn clean [_]
+      (b/delete {:path "target"}))
 
-(defn print-classpath []
-  (let [classpath (System/getProperty "java.class.path")]
-    (println "Current CLASSPATH:")
-    (println classpath)))
+(defn jar [_]
+      (b/write-pom {:class-dir class-dir
+                    :lib lib
+                    :version version
+                    :basis @basis
+                    :src-dirs ["src"]})
+      (b/copy-dir {:src-dirs ["src" "resources"]
+                   :target-dir class-dir})
+      (b/jar {:class-dir class-dir
+              :jar-file  src-jar-file}))
 
+(defn uberjar [_]
+      (clean nil)
+      (b/copy-dir {:src-dirs ["src" "resources"]
+                   :target-dir class-dir})
+      (b/compile-clj {:basis @basis
+                      :class-dir class-dir})
+      (b/uber {:class-dir class-dir
+               :uber-file uberjar-file
+               :basis @basis}))
 
-(defn ci "Run the CI pipeline of tests (and build the uberjar)." [opts]
-  (-> opts
-      (assoc :lib lib :version version :transitive true)
-      (bb/run-tests)
-      (bb/clean)
-      (bb/uber)))
-
-(defn uberjar "Just build the uberjar" [opts]
-      (-> opts
-          (assoc :lib lib :version version)
-          (bb/clean)
-          (bb/uber)))
+(defn run-tests [_]
+  (b/copy-dir {:src-dirs ["resources"]
+               :target-dir class-dir})
+  (b/compile-clj {:basis @basis
+                  :class-dir class-dir})
+  (let [{:keys [exit] :as res} (b/process {:command-args ["clj" "-M:test:test-dependencies"]})]
+    (when-not (zero? exit)
+      (throw (ex-info (str "run-tests failed") res)))))
